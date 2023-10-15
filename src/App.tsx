764cdaa6618b0dfcdb755e54f4ee6f3b43a5a520
matiasmjcs <matiasmejiascisternas@gmail.com>
2023-10-15 18:31:11 -0300

    #	modified:   src/App.tsx #	modified:   src/components/Footer.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css'
 import { Outlet } from 'react-router-dom'
 import NavBarComponent from './components/NavBar'
+import Footer from './components/Footer'
 function App() {
 
   return (
     <>
      <NavBarComponent/>
      <Outlet />
+     <Footer/>
     </>
   )
 }
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,23 @@
-export const Footer = () => {
-    return (
-        <footer>
-            
-        </footer>
-    )
-}
+const Footer = () => {
+  return (
+    <footer className="bg-[#1976D2] py-8 text-white">
+      <section className="container mx-auto flex flex-col sm:flex-row justify-between items-center">
+        <article className="w-5/6 p-2">
+          <h2 className="text-2xl font-bold">Personal Project</h2>
+          <p className="text-sm mt-2">
+            This is a personal project to showcase my skills. The backend is hosted on Render on a shared server and goes to sleep when inactive, so it may take up to 30 seconds to wake up.
+          </p>
+        </article>
+        <article className="w-5/6 p-2">
+          <h2 className="text-2xl font-bold">Contact</h2>
+          <ul className="mt-2">
+            <li className="text-sm">Email: matiasmejiascisternas@gmail.com</li>
+            <li className="text-sm">LinkedIn: <a target="_blank" href="https://www.linkedin.com/in/matias-mejias-cisternas" className="text-blue-300 hover:underline">Your Profile</a></li>
+          </ul>
+        </article>
+      </section>
+    </footer>
+  );
+};
+
+export default Footer;
