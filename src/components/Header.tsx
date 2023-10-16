ad4fbbb2babf544758663b9747c9fc35bdcf21ce
matiasmjcs <matiasmejiascisternas@gmail.com>
2023-10-16 18:04:08 -0300

    #	modified:   src/components/Header.tsx #	modified:   src/components/formHeader.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,8 +2,8 @@ import { FormHeader } from "./formHeader"
 
 export const Header = () => {
     return (
-        <header className="w-full p-7 h-[700px] flex flex-col header ">
-            <h1 className="text-4xl relative top-20 font-bold p-2 text-start mx-4 mt-3 text-slate-600">
+        <header className="w-full p-2 h-[700px] flex flex-col header ">
+            <h1 className="text-4xl relative top-20 font-bold p-2 text-start mt-3 text-slate-600">
                <span className="text-white">Your Luxury Seaside Escape.</span> 
             </h1>
             <FormHeader/>
