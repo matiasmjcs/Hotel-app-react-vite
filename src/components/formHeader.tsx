1c4901efe61ffca3cc3a8d1c5f5734c3ce8aa271
matiasmjcs <matiasmejiascisternas@gmail.com>
2023-10-16 18:33:17 -0300

    #	modified:   index.html #	modified:   src/components/formHeader.tsx #

--- src/components/formHeader.tsx
+++ src/components/formHeader.tsx
@@ -11,8 +11,16 @@ export const FormHeader = () => {
                     </label>
                     <input id="search" placeholder="Search" className="bg-transparent focus:outline-none" type="text" />
                 </article>
-                <input value="check in" className="bg-white rounded-xl text-black px-2 w-5/6 md:w-[150px]" type="date" />
-                <input value="check out" className="bg-white rounded-xl text-black px-2 w-5/6 md:w-[150px]" type="date" />
+                <article className='flex gap-2 text-base font-sans font-semibold flex-col md:flex-row w-5/6 md:w-[150px]'>
+                    <label htmlFor="checkin">Check in:</label>
+                    <input id="checkin" value="check in" className="rounded-xl px-2 " type="date" />
+                </article>
+
+                <article className='flex gap-2 text-base font-sans font-semibold flex-col md:flex-row w-5/6 md:w-[150px]'>
+                    <label htmlFor="checkout">Check out:</label>
+                    <input id="checkout" value="check out" className="rounded-xl px-2 " type="date" />
+                </article>
+
                 <article className='flex flex-row gap-2 items-center w-5/6 md:w-[150px]'>
                     <PersonIcon className="text-slate-600" />
                     <select className="bg-transparent px-2">
