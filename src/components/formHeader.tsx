f95c6f45d317a532e97a0d635bd5c43726b5a522
matiasmjcs <matiasmejiascisternas@gmail.com>
2023-10-16 18:09:43 -0300

    #	modified:   src/components/formHeader.tsx

--- src/components/formHeader.tsx
+++ src/components/formHeader.tsx
@@ -5,15 +5,15 @@ export const FormHeader = () => {
         <>
             <h2 className="text-xl font-bold p-2 text-white relative top-24">Choose your destination</h2>
             <form className="grid py-3 grid-cols-1 grid-rows-4 md:flex md:h-[50px] justify-items-center p-2 w-full  relative top-28 gap-3 transparent rounded-lg justify-between">
-                <article className='flex flex-row gap-2 w-[200px] md:w-[150px]'>
+                <article className='flex flex-row gap-2 w-5/6 md:w-[150px] my-3 md:my-0'>
                     <label className='flex items-center' htmlFor="search">
                         <SearchIcon className="text-slate-600" />
                     </label>
                     <input id="search" placeholder="Search" className="bg-transparent focus:outline-none" type="text" />
                 </article>
-                <input id="checkin" value="2024-01-01" className="transparent rounded-xl text-black px-2 w-[200px] md:w-[150px]" type="date" />
-                <input id="checkout" value="2024-01-01" className="transparent rounded-xl text-black px-2 w-[200px] md:w-[150px]" type="date" />
-                <article className='flex flex-row gap-2 items-center w-[200px] md:w-[150px]'>
+                <input id="checkin" value="2024-01-01" className="transparent rounded-xl text-black px-2 w-5/6 md:w-[150px]" type="date" />
+                <input id="checkout" value="2024-01-01" className="transparent rounded-xl text-black px-2 w-5/6 md:w-[150px]" type="date" />
+                <article className='flex flex-row gap-2 items-center w-5/6 md:w-[150px]'>
                     <PersonIcon className="text-slate-600" />
                     <select className="bg-transparent px-2">
                         <option className='text-slate-600' disabled selected>1 room 2 adults</option>
@@ -21,7 +21,7 @@ export const FormHeader = () => {
                         <option>2</option>
                     </select>
                 </article>
-                <button className='md:bg-white w-[200px] md:w-[130px] bg-blue-600 text-white md:text-blue-600 font-semibold rounded-2xl justify-self-center md:justify-self-end'>Search</button>
+                <button className='md:bg-white w-5/6 md:w-[130px] py-2 md:p-0 bg-blue-600 text-white md:text-blue-600 font-semibold rounded-2xl justify-self-center md:justify-self-end'>Search</button>
             </form>
         </>
     )
